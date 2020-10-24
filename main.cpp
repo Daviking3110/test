bc073846f13a9a280b6f1f84595e0a73a25fb516
Anthony <anthony_31-10@hotmail.com>
2020-10-24 14:38:23 +0200

    test 1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : <nom du fichier>.ccp
+Auteur(s) : Anthony David
+Date creation : <jj.mm.aaaa>
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int a=36;
+cout << a ;
+return EXIT_SUCCESS;
+}
